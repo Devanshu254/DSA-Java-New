7a3a05196728c1cc42d5a1821d6663a571c7ddba
Devanshu254 <devanshujha8305@gmail.com>
2025-09-27 00:47:49 +0530

    initial commit

---
+++ Java Collection Frameworks/Core Interfaces/Geeks.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Geeks {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("java");
+        for(String lang: list) {
+            System.out.println(lang);
+        }
+    }
+}
