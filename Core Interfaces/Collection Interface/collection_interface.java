25251e6123e32958df1a015cb12057001422666a
Devanshu254 <devanshujha8305@gmail.com>
2025-09-27 00:55:38 +0530

    collections

---
+++ Core Interfaces/Collection Interface/collection_interface.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class collection_interface {
+    public static void main(String[] args) {
+        Collection<String> c = new ArrayList<>();
+        c.add("apple");
+        c.add("banana");
+        c.add("Orange");
+
+        System.out.println(c);
+    }
+}
